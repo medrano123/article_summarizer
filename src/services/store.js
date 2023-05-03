7f1055c82b114f64d3ff72470861a4e503e6b719
Giovanni Medrano <giovannimedrano@Giovannis-MBP.hitronhub.home>
2023-05-03 11:21:02 -0500

    almost done

--- src/services/store.js
+++ src/services/store.js
@@ -1,6 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 
+import { articleApi } from "./article";
+
 export const store = configureStore({
-    reducer: {}, // allows us to get a specific slice of the cake
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat()
+    reducer: {
+        [articleApi.reducerPath]: articleApi.reducer
+    }, // allows us to get a specific slice of the cake
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(articleApi.middleware)
 });
